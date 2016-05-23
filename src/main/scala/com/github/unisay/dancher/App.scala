8e833fc19ea6c59fd4dd837cf505de60e07737cf
Yuriy Lazaryev <Yuriy.Lazaryev@zalando.de>
2016-05-23 19:48:17 +0200

    Ops syntax for action runner

--- src/main/scala/com/github/unisay/dancher/App.scala
+++ src/main/scala/com/github/unisay/dancher/App.scala
@@ -6,27 +6,32 @@ import Action._
 
 object App extends JSApp {
 
+  implicit val actionRunner = new ActionRunner()
+
+  implicit class ActionFOps[E](action: ActionF[E]) {
+    def runAction(implicit runner: ActionRunner): E = runner.run(action)
+  }
+
+  def updateHorizontalLayout(model: Widget): ActionF[DomElement] = ???
+
+  lazy val model: Widget = VerticalLayout(
+    Button("Add Label", Some(_ ⇒ updateHorizontalLayout(model))),
+    HorizontalLayout(
+      Label("Horizontally"),
+      Label("Placed"),
+      Label("Labels")
+    )
+  )
+
   @JSExport
   override def main(): Unit = {
-
-    def updateHorizontalLayout(model: Widget): ActionF[DomElement] = ???
-
-    lazy val model: Widget = VerticalLayout (
-      Button("Add Label", Some(_ ⇒ updateHorizontalLayout(model))),
-      HorizontalLayout (
-        Label("Horizontally"),
-        Label("Placed"),
-        Label("Labels")
-      )
-    )
-
-    new ActionRunner().run {
-      for {
-        body ← getDocumentBody
-        layout ← model.create
-        _ ← body appendChild layout
-      } yield ()
-    }
+    val init = for {
+      body ← getDocumentBody
+      layout ← model.create
+      _ ← body appendChild layout
+    } yield ()
+    init.runAction
   }
+
 }
 
