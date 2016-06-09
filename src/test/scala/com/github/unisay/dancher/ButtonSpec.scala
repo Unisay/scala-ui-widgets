1faf7471ad8ecaa5fae4d6402a6509943f6098b7
Yuriy Lazarev <Yuriy.Lazarev@gmail.com>
2016-06-09 23:00:47 +0200

    ButtonSpec

---
+++ src/test/scala/com/github/unisay/dancher/ButtonSpec.scala
@@ -0,0 +1,26 @@
+package com.github.unisay.dancher
+
+import com.github.unisay.dancher.Arbitraries._
+import com.github.unisay.dancher.Matchers._
+import org.specs2.ScalaCheck
+import org.specs2.mutable.Specification
+
+class ButtonSpec extends Specification with ScalaCheck {
+
+  "Button" should {
+
+    "create" in prop { (button: Button) ⇒
+      button.create must beActionAsScript(
+        s"""
+        |var button1 = document.createElement('button')
+        |button1.setAttribute('id', '${button.domId.value}')
+        |button1.setAttribute('class', 'd-button')
+        |var text2 = document.createTextNode('${button.label}')
+        |button1.appendChild(text2)
+        |/* SetOnClick(button1) */
+        """
+      )
+    }
+
+  }
+}
